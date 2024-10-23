73deff22b3c29e5fdaf757c511033654081a9f8a
Dmitry Dubina <dmitryd.prog@gmail.com>
2024-10-23 11:59:47 +0700

    Home without cmd modifier

--- keyboards/keychron/k3_pro/dmr.c
+++ keyboards/keychron/k3_pro/dmr.c
@@ -4,22 +4,31 @@
 __attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  uint8_t mod_state = get_mods();
   switch(keycode) {
   case KC_LEFT:
-    if (record->event.pressed && get_mods() == MOD_BIT(KC_LCMD)) {
-      register_code(KC_HOME);
+    if (mod_state & MOD_LGUI) {
+      del_mods(MOD_LGUI);
+      unregister_code(KC_LEFT);
+      if (record->event.pressed) register_code(KC_HOME);
+      else unregister_code(KC_HOME);
+      add_mods(MOD_LGUI);
+
       return false;
     } else {
-      unregister_code(KC_HOME);
       return true;
     }
     break;
   case KC_RIGHT:
-    if (record->event.pressed && get_mods() == MOD_BIT(KC_LCMD)) {
-      register_code(KC_END);
+    if (mod_state & MOD_LGUI) {
+      del_mods(MOD_LGUI);
+      unregister_code(KC_RIGHT);
+      if (record->event.pressed) register_code(KC_END);
+      else unregister_code(KC_END);
+      add_mods(MOD_LGUI);
+
       return false;
     } else {
-      unregister_code(KC_END);
       return true;
     }
     break;
